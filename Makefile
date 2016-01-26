70470c76629abed6a3d0de0ce3617768f37e7dbc
Bastian Kleineidam <bastian.kleineidam@web.de>
2016-01-26 20:29:48 +0100

    Switch to keepasslib for .kdbx (keepass v4) support.

--- Makefile
+++ Makefile
@@ -1,46 +1,23 @@
 # This Makefile is only used by developers.
 PYVER:=2.7
 PYTHON:=python$(PYVER)
-ifeq ($(shell uname),Darwin)
-  NUMPROCESSORS:=$(shell sysctl -a | grep machdep.cpu.core_count | cut -d " " -f 2)
-  CHMODMINUSMINUS:=
-else
-  NUMPROCESSORS:=$(shell grep -c processor /proc/cpuinfo)
-  CHMODMINUSMINUS:=--
-endif
-# Pytest options:
-# - use multiple processors
-# - write test results in file
-# - run all tests found in the "tests" subdirectory
-PYTESTOPTS:=-n $(NUMPROCESSORS) --resultlog=testresults.txt
+VERSION:=$(shell $(PYTHON) setup.py --version)
+APPNAME:=$(shell $(PYTHON) setup.py --name)
 
-DEBUILD_AREA:=$(HOME)/src/build-area
 
 PY_FILES:=\
 	setup.py \
 	keepassc \
 	keepasslib/__init__.py \
-	keepasslib/baker.py \
-	keepasslib/decorators.py \
-	keepasslib/header.py \
-	keepasslib/hier.py \
-	keepasslib/infoblock.py \
-	keepasslib/kpdb.py
-
-PY2APPOPTS?=
-
-MANIFEST: MANIFEST.in setup.py
-	$(PYTHON) setup.py sdist --manifest-only
+	keepasslib/baker.py
 
 chmod:
 	-chmod -R a+rX,u+w,go-w $(CHMODMINUSMINUS) *
 	find . -type d -exec chmod 755 {} \;
 
-dist: MANIFEST chmod
-	$(PYTHON) setup.py sdist --formats=bztar
-
-app: chmod
-	$(PYTHON) setup.py py2app $(PY2APPOPTS)
+dist: chmod
+	$(PYTHON) setup.py sdist --formats=tar bdist_wheel
+	gzip --best dist/$(APPNAME)-$(VERSION).tar
 
 doc/keepassc.1.html: doc/keepassc.1
 	man2html -r $< | tail -n +2 | sed 's/Time:.*//g' | sed 's@/:@/@g' > $@
@@ -58,10 +35,4 @@ check:
 pyflakes:
 	pyflakes $(PY_FILES)
 
-test:
-	$(PYTHON) -m pytest $(PYTESTOPTS) $(TESTOPTS) $(TESTS)
-
-deb:
-	git-buildpackage --git-export-dir=$(DEBUILD_AREA) --git-upstream-branch=master --git-debian-branch=debian  --git-ignore-new
-
-.PHONY: dist chmod check pyflakes doccheck test deb
+.PHONY: dist chmod check pyflakes doccheck
--- setup.py
+++ setup.py
@@ -2,76 +2,16 @@
 # -*- coding: iso-8859-1 -*-
 # Copyright (c) 2012 Bastian Kleineidam
 
-import sys
-import glob
-import os
-try:
-    # setuptools (which is needed by py2app) monkey-patches the
-    # distutils.core.Command class.
-    # So we need to import it before importing the distutils core
-    import setuptools
-except ImportError:
-    # ignore when setuptools is not installed
-    pass
-from distutils.core import setup
+from setuptools import setup
 from distutils.command.register import register
-from distutils import util
-try:
-    import py2exe
-except ImportError:
-    # ignore when py2exe is not installed
-    pass
 
 appname = 'python-keepass'
 version = '0.2'
-description = "Command line and python interface for KeePass v3 files."
+description = "Command line interface for KeePass v4 files."
 long_description = """
-A command line and Python interface for operating on
-files in KeePass format v3 (used by KeePass 1.x, and KeePassX).
+A command line interface to read from file in KeePass format v4
+(used by KeePass and KeePassx 2.x).
 """
-
-# basic excludes for py2exe
-py_excludes = ['doctest', 'unittest', 'optcomplete', 'Tkinter']
-# py2exe options for windows .exe packaging
-py2exe_options = dict(
-    packages=["encodings"],
-    excludes=py_excludes + ['win32com.gen_py'],
-    # silence py2exe error about not finding msvcp90.dll
-    dll_excludes=['MSVCP90.dll'],
-    compressed=1,
-    optimize=2,
-)
-# py2app options for OSX packaging
-py2app_options = dict(
-    excludes=py_excludes,
-    strip=True,
-    optimize=2,
-    argv_emulation=False,
-)
-
-
-def get_nt_platform_vars ():
-    """Return program file path and architecture for NT systems."""
-    platform = util.get_platform()
-    if platform == "win-amd64":
-        # the Visual C++ runtime files are installed in the x86 directory
-        progvar = "%ProgramFiles(x86)%"
-        architecture = "amd64"
-    elif platform == "win32":
-        progvar = "%ProgramFiles%"
-        architecture = "x86"
-    else:
-        raise ValueError("Unsupported platform %r" % platform)
-    return os.path.expandvars(progvar), architecture
-
-
-def add_msvc_files (files):
-    """Add needed MSVC++ runtime files."""
-    dirname = "Microsoft.VC90.CRT"
-    prog_dir, architecture = get_nt_platform_vars()
-    p = r'%s\Microsoft Visual Studio 9.0\VC\redist\%s\%s\*.*'
-    args = (prog_dir, architecture, dirname)
-    files.append((dirname, glob.glob(p % args)))
 
 
 class MyRegister (register, object):
@@ -92,7 +32,7 @@ args = dict(
     description = description,
     long_description = long_description,
     maintainer = 'Bastian Kleineidam',
-    maintainer_email = 'calvin@users.sourceforge.net',
+    maintainer_email = 'bastian.kleineidam@web.de',
     packages = ['keepasslib'],
     scripts = ['keepassc'],
     data_files = [],
@@ -111,16 +51,6 @@ args = dict(
         'Topic :: Security :: Cryptography',
         'Topic :: Utilities'
     ],
-    options = {
-        "py2exe": py2exe_options,
-        "py2app": py2app_options,
-    }
 )
-
-if 'py2exe' in sys.argv[1:]:
-    add_msvc_files(args['data_files'])
-    args['console'] = ['keepassc']
-if sys.platform == 'darwin':
-    args["app"] = ['keepassc']
 
 setup(**args)
